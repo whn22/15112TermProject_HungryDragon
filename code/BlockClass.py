98259963d476edcad141c85501fe1bf02585d41f
Sunny Wu <3033518286@qq.com>
2022-12-03 11:13:02 -0500

    able to move blocks

--- code/BlockClass.py
+++ code/BlockClass.py
@@ -22,15 +22,26 @@ class MovBlock(Block):
         super().__init__(x, y, w, h, color)
         self.dx = -1
         self.dy = -1
+        self.hold = False
 
     def findDiff(self, app):
-        self.dx, self.dy = self.x - app.nightFury.x, self.y - app.nightFury.y
+        self.dx, self.dy = self.x - app.nightFury.x,\
+                           self.y - app.nightFury.y
 
     # if key pressed "Space"
     def move(self, app):
-        if self.isObjectTouch(app.nightFury):
-            backup = self.x, self.y
-            self.resetLocation(app.nightFury.x + self.dx, 
-                                app.nightFury.y + self.dy)
-            if self.withinCanvasRange(app) == False:
+        backup = self.x, self.y
+        # self.findDiff(app)
+        # print(self.dx, self.dy)
+        self.resetLocation((app.nightFury.x + self.dx, 
+                            app.nightFury.y + self.dy))
+        for block in app.terrain:
+            if type(block) == Platform and self.isObjectCollide(block):
                 self.x, self.y = backup
+                self.hold = False
+        if self.withinCanvasRange(app) == False:
+            self.x, self.y = backup
+    
+class Platform(Block):
+    def __init__(self, x, y, w, h, color):
+        super().__init__(x, y, w, h, color)
